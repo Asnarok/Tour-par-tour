0718d1322e132de7585f032ff4069740d623a8eb
Asnarok <matteo.chanourdie@gmail.com>
2021-04-29 10:52:07 +0200

    kk
    Co-authored-by: aurelien.girin@gmail.com <aurelien.girin@gmail.com>

--- jeu/Jeu.java
+++ jeu/Jeu.java
@@ -2,7 +2,7 @@ package jeu;
 
 public class Jeu {
     public static void main (String[] args){
-        
+        System.out.println("Hello world");
     }
     
 }
--- jeu/Perso.java
+++ jeu/Perso.java
@@ -2,4 +2,6 @@ package jeu;
 
 public class Perso {
     
+    
+
 }
