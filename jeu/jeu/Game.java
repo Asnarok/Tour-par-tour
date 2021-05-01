19afa2f5c6c64872238adb60a21ddd5ef46295a3
Asnarok <matteo.chanourdie@gmail.com>
2021-05-01 19:27:53 +0200

    package jeu + Newton

---
+++ jeu/jeu/Game.java
@@ -0,0 +1,17 @@
+package jeu;
+
+public class Game {
+
+    public static Character player1, player2;
+
+    //personnages
+    public static final Character newton = new Character("I. Newton", new Type[]{Type.GRAVITATION}, 
+                                                        new Type[]{Type.QUANTIC, Type.COSMOS},
+                                                        new String[]{"Développement binomial", "Superpesanteur"},
+                                                        new Type[]{Type.MATH, Type.GRAVITATION});
+
+    public static void main (String[] args){
+        
+    }
+    
+}
