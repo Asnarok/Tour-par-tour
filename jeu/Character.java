96ae78f27e14f9673125a9e6878ae248f2dbe7ff
Asnarok <matteo.chanourdie@gmail.com>
2021-05-01 18:33:07 +0200

    enum2

--- jeu/Character.java
+++ jeu/Character.java
@@ -1,13 +1,16 @@
 package jeu;
+
+public static enum Type {
+    ELECTRIC, COSMOS, INFORMATIC, GEOMETRIC, MATTER, RADIO;
+}
+
 public class Character {
     
     //types
     public static final int ELECTRIC = 0, COSMOS = 1, INFORMATIC = 2, GEOMETRIC = 3, MATTER = 4, RADIO = 5;
     public static final int HP_CONST = 100;
 
-    public static enum Type {
-        ELECTRIC, COSMOS, INFORMATIC, GEOMETRIC, MATTER, RADIO;
-    }
+    
 
     private String name;
     private float HP;
